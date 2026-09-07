84b5bcb4325dde88b81f4499c3888f6189706646
danikrakatau1 <danikrakatau1@gmail.com>
2026-09-08 05:01:51 +0700

    Add Bali lab preview route adapter

---
+++ lab-bali01-route-v0.js
@@ -0,0 +1,7 @@
+(() => {
+  'use strict';
+  const path = location.pathname.replace(/^\/+|\/+$/g, '').toLowerCase();
+  if (path === 'p_5a261fd0a07697fb2c7d2a9a053679c3') {
+    location.replace('/editor/webgl-lab/bali01/index.html');
+  }
+})();
